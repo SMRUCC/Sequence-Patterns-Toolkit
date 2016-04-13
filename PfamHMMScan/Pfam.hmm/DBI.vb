b9a20237d61c46fa5a2dc894fdaed5bff496b558
xieguigang <xie.guigang@gmail.com>
2016-04-13 13:28:34 +0800

    add doc

--- PfamHMMScan/Pfam.hmm/DBI.vb
+++ PfamHMMScan/Pfam.hmm/DBI.vb
@@ -14,7 +14,9 @@ Public Module DBI
     End Function
 
     ''' <summary>
-    ''' Retrieve hidden Markov model (HMM) profile from PFAM database
+    ''' HMMStruct = gethmmprof(PFAMNumber) determines a protein family accession number from PFAMNumber (an integer), 
+    ''' searches the PFAM database for the associated record, retrieves the HMM profile information, 
+    ''' and stores it in HMMStruct, a MATLAB structure.
     ''' </summary>
     ''' <param name="PFAMNumber">
     ''' Integer specifying a protein family number of an HMM profile record in the PFAM database. For example, 2 is the protein family number for the protein family 'PF00002'.
