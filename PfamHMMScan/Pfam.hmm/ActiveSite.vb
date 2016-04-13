5724efcbfd92877bb8f94b5a412a5ef0e27c9b57
xieguigang <xie.guigang@gmail.com>
2016-04-13 22:31:33 +0800

    finish active site parser

--- PfamHMMScan/Pfam.hmm/ActiveSite.vb
+++ PfamHMMScan/Pfam.hmm/ActiveSite.vb
@@ -1,5 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
 Imports Microsoft.VisualBasic.Serialization
+Imports Microsoft.VisualBasic.Linq
+Imports System.Text.RegularExpressions
 
 ''' <summary>
 ''' active_site.dat
@@ -24,7 +26,39 @@ Public Structure ActiveSite : Implements sIdEnumerable
     End Function
 
     Public Shared Function StreamParser(stream As String()) As ActiveSite
+        Dim LQuery = (From s As String
+                      In stream
+                      Let tag As String = s.Substring(0, 2)
+                      Let value As String = s.Substring(4)
+                      Select tag,
+                          value
+                      Group By tag Into Group) _
+                              .ToDictionary(Function(x) x.tag,
+                                            Function(x) x.Group.ToArray(Function(o) o.value))
+        Dim pfam As New ActiveSite With {
+            .ID = LQuery.TryGetValue(NameOf(pfam.ID)).DefaultFirst,
+            .RE = __RLhash(LQuery.TryGetValue(NameOf(pfam.RE))),
+            .AL = (From s As String
+                   In LQuery.TryGetValue(NameOf(pfam.AL)).AsParallel
+                   Select New Alignment(s)).ToArray
+        }
 
+        Return pfam
+    End Function
+
+    Private Shared Function __RLhash(value As String()) As Dictionary(Of String, RE)
+        Dim LQuery = (From x As String In value
+                      Let tokens As String() = Strings.Split(x, "  ")
+                      Let id As String = tokens(Scan0)
+                      Let int As Integer = Scripting.CTypeDynamic(Of Integer)(tokens(1))
+                      Select id,
+                          int
+                      Group By id Into Group) _
+                              .ToDictionary(Function(x) x.id,
+                                            Function(x) New RE With {
+                                                .ID = x.id,
+                                                .Value = x.Group.ToArray(Function(t) t.int)})
+        Return LQuery
     End Function
 End Structure
 
@@ -40,6 +74,12 @@ End Structure
 Public Structure Alignment : Implements sIdEnumerable
     Public Property ID As String Implements sIdEnumerable.Identifier
     Public Property MAL As String
+
+    Sub New(s As String)
+        Dim tokens As String() = Regex.Split(s, "\s+")
+        ID = tokens(Scan0)
+        MAL = tokens(1)
+    End Sub
 
     Public Overrides Function ToString() As String
         Return Me.GetJson
--- PfamHMMScan/Program.vb
+++ PfamHMMScan/Program.vb
@@ -2,6 +2,7 @@
 
     Public Function Main() As Integer
         Dim sss = Stockholm.DocParser("G:\GCModeller\PfamFamily\Pfam-A.hmm\Pfam-A.hmm.dat").ToArray
+        Dim gggg = ActiveSite.LoadStream("G:\GCModeller\PfamFamily\Pfam-A.hmm\active_site.dat").ToArray
 
         Return GetType(CLI).RunCLI(App.CommandLine)
     End Function
