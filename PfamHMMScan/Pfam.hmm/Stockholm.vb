16b6f486eb8c0e8fe204b130e2c0e60c273ad1de
xieguigang <xie.guigang@gmail.com>
2016-04-13 21:20:09 +0800

    finish the Stockholm parser

--- PfamHMMScan/Pfam.hmm/Stockholm.vb
+++ PfamHMMScan/Pfam.hmm/Stockholm.vb
@@ -1,6 +1,8 @@
-﻿Imports Microsoft.VisualBasic.Linq
+﻿Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Serialization
 
-Public Class Stockholm
+Public Class Stockholm : Implements sIdEnumerable
 
     ''' <summary>
     ''' Identifier
@@ -11,7 +13,7 @@ Public Class Stockholm
     ''' Pfam accession ID
     ''' </summary>
     ''' <returns></returns>
-    Public Property AC As String
+    Public Property AC As String Implements sIdEnumerable.Identifier
     ''' <summary>
     ''' Definition
     ''' </summary>
@@ -25,7 +27,11 @@ Public Class Stockholm
     Public Property TP As String
     Public Property ML As Integer
     Public Property CL As String
-    Public Property NE As String
+    Public Property NE As String()
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
 
     Public Shared Iterator Function DocParser(path As String) As IEnumerable(Of Stockholm)
         Dim lines As String() = path.ReadAllLines
@@ -34,22 +40,22 @@ Public Class Stockholm
         VBDebugger.Mute = True
 
         For Each token As String() In tokens
-            Dim hash As Dictionary(Of String, String) = __hash(token)
+            Dim hash As Dictionary(Of String, String()) = __hash(token.Skip(1))
             Dim x As New Stockholm
 
-            x.AC = hash.TryGetValue(NameOf(x.AC))
-            x.CL = hash.TryGetValue(NameOf(x.CL))
-            x.DE = hash.TryGetValue(NameOf(x.DE))
+            x.AC = hash.TryGetValue(NameOf(x.AC)).DefaultFirst
+            x.CL = hash.TryGetValue(NameOf(x.CL)).DefaultFirst
+            x.DE = hash.TryGetValue(NameOf(x.DE)).DefaultFirst
 
-            Dim tmp As String = hash.TryGetValue(NameOf(x.GA))
+            Dim tmp As String = hash.TryGetValue(NameOf(x.GA)).DefaultFirst
             If Not String.IsNullOrEmpty(tmp) Then
                 x.GA = Strings.Split(tmp, ";").ToArray(Function(s) Val(s), where:=Function(s) Not s.IsBlank)
             End If
 
-            x.ID = hash.TryGetValue(NameOf(x.ID))
-            x.ML = Scripting.CTypeDynamic(Of Integer)(hash.TryGetValue(NameOf(x.ML)))
+            x.ID = hash.TryGetValue(NameOf(x.ID)).DefaultFirst
+            x.ML = Scripting.CTypeDynamic(Of Integer)(hash.TryGetValue(NameOf(x.ML)).DefaultFirst)
             x.NE = hash.TryGetValue(NameOf(x.NE))
-            x.TP = hash.TryGetValue(NameOf(x.TP))
+            x.TP = hash.TryGetValue(NameOf(x.TP)).DefaultFirst
 
             Yield x
         Next
@@ -57,7 +63,16 @@ Public Class Stockholm
         VBDebugger.Mute = False
     End Function
 
-    Private Shared Function __hash(token As String()) As Dictionary(Of String, String)
+    Private Shared Function __hash(token As IEnumerable(Of String)) As Dictionary(Of String, String())
+        Dim LQuery = (From s As String
+                      In token
+                      Let ss As String = s.Replace("#=GF ", "")
+                      Let ts As String() = Strings.Split(ss, "   ")
+                      Select ts.First, ts.Last
+                      Group By First Into Group)
 
+        Dim hash As Dictionary(Of String, String()) =
+            LQuery.ToDictionary(Function(x) x.First, Function(x) x.Group.ToArray(Function(ts) ts.Last))
+        Return hash
     End Function
 End Class
--- PfamHMMScan/Program.vb
+++ PfamHMMScan/Program.vb
@@ -1,6 +1,8 @@
 ﻿Module Program
 
     Public Function Main() As Integer
+        Dim sss = Stockholm.DocParser("G:\GCModeller\PfamFamily\Pfam-A.hmm\Pfam-A.hmm.dat").ToArray
+
         Return GetType(CLI).RunCLI(App.CommandLine)
     End Function
 End Module
