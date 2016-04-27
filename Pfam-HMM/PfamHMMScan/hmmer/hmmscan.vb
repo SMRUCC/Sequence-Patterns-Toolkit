9ceed7f745c114f9af9dedb1097819daf9ee1e09
xieguigang <xie.guigang@gcmodeller.org>
2016-04-27 16:42:25 +0800

    finish the hmmscan documnent parser

--- Pfam-HMM/PfamHMMScan/hmmer/hmmscan.vb
+++ Pfam-HMM/PfamHMMScan/hmmer/hmmscan.vb
@@ -3,145 +3,195 @@ Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Serialization
 
-''' <summary>
-''' hmmscan :: search sequence(s) against a profile database
-''' </summary>
-Public Class hmmscan
+Namespace hmmscan
 
     ''' <summary>
-    ''' HMMER 3.1b1 (May 2013); http://hmmer.org/
+    ''' hmmscan :: search sequence(s) against a profile database
     ''' </summary>
-    ''' <returns></returns>
-    Public Property version As String
-    ''' <summary>
-    ''' query sequence file
-    ''' </summary>
-    ''' <returns></returns>
-    Public Property query As String
-    ''' <summary>
-    ''' target HMM database
-    ''' </summary>
-    ''' <returns></returns>
-    Public Property HMM As String
-    Public Property Querys As Query()
+    Public Class hmmscan
 
-    Public Overrides Function ToString() As String
-        Return New With {version, query, HMM}.GetJson
-    End Function
+        ''' <summary>
+        ''' HMMER 3.1b1 (May 2013); http://hmmer.org/
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property version As String
+        ''' <summary>
+        ''' query sequence file
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property query As String
+        ''' <summary>
+        ''' target HMM database
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property HMM As String
+        Public Property Querys As Query()
 
-    Public Function GetTable() As ScanTable()
-        Return LinqAPI.Exec(Of ScanTable) <= From query As Query
-                                             In Querys
-                                             Select result = query.GetTable
-    End Function
-End Class
+        Public Overrides Function ToString() As String
+            Return New With {version, query, HMM}.GetJson
+        End Function
 
-''' <summary>
-''' Scores for complete sequence (score includes all domains)
-''' </summary>
-Public Class Query : Inherits ClassObject
-
-    Public Property name As String
-    Public Property length As Integer
-    Public Property Hits As Hit()
-    ''' <summary>
-    ''' ------ inclusion threshold ------
-    ''' </summary>
-    ''' <returns></returns>
-    Public Property uncertain As Hit()
-
-    Public Overrides Function ToString() As String
-        Return $"{name}  [L={length}]"
-    End Function
-
-    Public Function GetTable() As ScanTable()
-        Return Hits.ToList(Function(x) New ScanTable(Me, x)) + From x As Hit
-                                                               In uncertain.SafeQuery
-                                                               Select New ScanTable(Me, x)
-    End Function
-End Class
-
-Public Class ScanTable
-    Public Property name As String
-    Public Property len As Integer
-    <Column(Name:="full.E-value")> Public Property FullEvalue As Double
-    <Column(Name:="full.score")> Public Property FullScore As Double
-    <Column(Name:="full.bias")> Public Property FullBias As Double
-    <Column(Name:="best.E-value")> Public Property BestEvalue As Double
-    <Column(Name:="best.score")> Public Property BestScore As Double
-    <Column(Name:="best.bias")> Public Property BestBias As Double
-    Public Property exp As Double
-    Public Property N As Integer
-    Public Property model As String
-    Public Property describ As String
-
-    Sub New()
-    End Sub
-
-    Sub New(query As Query, hit As Hit)
-        name = query.name
-        len = query.length
-        FullEvalue = hit.Full.Evalue
-        FullScore = hit.Full.score
-        FullBias = hit.Full.bias
-        BestBias = hit.Best.bias
-        BestEvalue = hit.Best.Evalue
-        BestScore = hit.Best.score
-        exp = hit.exp
-        N = hit.N
-        model = hit.Model
-        describ = hit.Description
-    End Sub
-
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
-
-Public Class Hit
+        Public Function GetTable() As ScanTable()
+            Return LinqAPI.Exec(Of ScanTable) <= From query As Query
+                                                 In Querys
+                                                 Select result = query.GetTable
+        End Function
+    End Class
 
     ''' <summary>
-    ''' --- full sequence ---
+    ''' Scores for complete sequence (score includes all domains)
     ''' </summary>
-    ''' <returns></returns>
-    Public Property Full As Score
-    ''' <summary>
-    ''' --- best 1 domain ---
-    ''' </summary>
-    ''' <returns></returns>
-    Public Property Best As Score
+    Public Class Query : Inherits ClassObject
+
+        Public Property name As String
+        Public Property length As Integer
+        Public Property Hits As Hit()
+        ''' <summary>
+        ''' ------ inclusion threshold ------
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property uncertain As Hit()
+        Public Property Alignments As Alignment()
+
+        Public Overrides Function ToString() As String
+            Return $"{name}  [L={length}]"
+        End Function
+
+        Public Function GetTable() As ScanTable()
+            Return Hits.ToList(Function(x) New ScanTable(Me, x, "!"c)) +
+            From x As Hit
+            In uncertain.SafeQuery
+            Select New ScanTable(Me, x, "?"c)
+        End Function
+    End Class
+
+    Public Class ScanTable
+
+        ''' <summary>
+        ''' !?
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property rank As Char
+        Public Property name As String
+        Public Property len As Integer
+        <Column(Name:="full.E-value")> Public Property FullEvalue As Double
+        <Column(Name:="full.score")> Public Property FullScore As Double
+        <Column(Name:="full.bias")> Public Property FullBias As Double
+        <Column(Name:="best.E-value")> Public Property BestEvalue As Double
+        <Column(Name:="best.score")> Public Property BestScore As Double
+        <Column(Name:="best.bias")> Public Property BestBias As Double
+        Public Property exp As Double
+        Public Property N As Integer
+        Public Property model As String
+        Public Property describ As String
+
+        Sub New()
+        End Sub
+
+        Sub New(query As Query, hit As Hit, ranks As Char)
+            name = query.name
+            len = query.length
+            FullEvalue = hit.Full.Evalue
+            FullScore = hit.Full.score
+            FullBias = hit.Full.bias
+            BestBias = hit.Best.bias
+            BestEvalue = hit.Best.Evalue
+            BestScore = hit.Best.score
+            exp = hit.exp
+            N = hit.N
+            model = hit.Model
+            describ = hit.Description
+            rank = ranks
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+
+    Public Class Hit
+
+        ''' <summary>
+        ''' --- full sequence ---
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Full As Score
+        ''' <summary>
+        ''' --- best 1 domain ---
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Best As Score
 
 #Region "-#dom-"
 
-    Public Property exp As Double
-    Public Property N As Integer
+        Public Property exp As Double
+        Public Property N As Integer
 #End Region
 
-    Public Property Model As String
-    Public Property Description As String
+        Public Property Model As String
+        Public Property Description As String
 
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Class
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
 
-Public Structure Score
+    Public Structure Score
 
-    <Column(Name:="E-value")> Public Property Evalue As Double
-    Public Property score As Double
-    Public Property bias As Double
+        <Column(Name:="E-value")> Public Property Evalue As Double
+        Public Property score As Double
+        Public Property bias As Double
 
-    Sub New(buf As String())
-        Call Me.New(buf(0), buf(1), buf(2))
-    End Sub
+        Sub New(buf As String())
+            Call Me.New(buf(0), buf(1), buf(2))
+        End Sub
 
-    Sub New(e As String, s As String, b As String)
-        Evalue = Val(e.Trim)
-        score = Val(s.Trim)
-        bias = Val(b.Trim)
-    End Sub
+        Sub New(e As String, s As String, b As String)
+            Evalue = Val(e.Trim)
+            score = Val(s.Trim)
+            bias = Val(b.Trim)
+        End Sub
 
-    Public Overrides Function ToString() As String
-        Return Me.GetJson
-    End Function
-End Structure
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Structure
+
+    Public Class Alignment
+
+        Public Property model As String
+        Public Property describ As String
+        Public Property Aligns As Align()
+
+        Public Overrides Function ToString() As String
+            Return $"{model}  {describ}"
+        End Function
+    End Class
+
+    Public Class Align
+
+        <Column(Name:="#")> Public Property rank As String
+        Public Property score As Double
+        Public Property bias As Double
+        <Column(Name:="c-Evalue")> Public Property cEvalue As Double
+        <Column(Name:="i-Evalue")> Public Property iEvalue As Double
+        Public Property hmmfrom As Integer
+        <Column(Name:="hmm To")> Public Property hmmTo As Integer
+        Public Property alifrom As Integer
+        <Column(Name:="ali To")> Public Property aliTo As Integer
+        Public Property envfrom As Integer
+        <Column(Name:="env To")> Public Property envTo As Integer
+        Public Property acc As Double
+
+        Sub New()
+        End Sub
+
+        Friend Sub New(buf As String())
+
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return Me.GetJson
+        End Function
+    End Class
+End Namespace
