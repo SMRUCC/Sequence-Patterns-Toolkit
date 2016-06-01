6f8a7e14528100c4ffc64dcbec73efa2b9830d45
xieguigang <xie.guigang@gcmodeller.org>
2016-06-01 16:13:11 +0800

    i knew how to

---
+++ SequencePatterns/LociExtensions.vb
@@ -0,0 +1,8 @@
+﻿Imports LANS.SystemsBiology.SequenceModel.NucleotideModels
+
+''' <summary>
+''' 将序列特征的搜索结果转换为<see cref="SimpleSegment"/>对象类型
+''' </summary>
+Public Module LociExtensions
+
+End Module
