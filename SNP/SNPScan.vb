0d70395a5690f65ae516b0c07994d6442b8f15e3
xieguigang <xie.guigang@gcmodeller.org>
2016-06-02 15:42:44 +0800

    config for snp project

--- SNP/SNPScan.vb
+++ SNP/SNPScan.vb
@@ -1,3 +1,25 @@
-﻿Public Module SNPScan
+﻿Imports System.Runtime.CompilerServices
+Imports LANS.SystemsBiology.SequenceModel
 
+Public Module SNPScan
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="nt">可以不经过任何处理，程序在这里会自动使用clustal进行对齐操作</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function ScanRaw(nt As FASTA.FastaFile) As SNP()
+
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="nt">序列必须都是已经经过clustal对齐了的</param>
+    ''' <returns></returns>
+    <Extension>
+    Public Function Scan(nt As FASTA.FastaFile) As SNP()
+
+    End Function
 End Module
