10390369001c5517eb1a4b04ea70e795868fd7e3
xieguigang <xie.guigang@gcmodeller.org>
2016-06-05 01:04:03 +0800

    tweaks on the batch search job

--- SequenceTools/CLI/PalindromeBatch.vb
+++ SequenceTools/CLI/PalindromeBatch.vb
@@ -12,7 +12,8 @@ Imports Microsoft.VisualBasic.Parallel.Linq
 Partial Module Utilities
 
     <ExportAPI("/Palindrome.BatchTask",
-               Usage:="/Palindrome.BatchTask /in <in.DIR> [/num_threads 4 /min 3 /max 20 /min-appears 2 /cutoff <0.6> /max-dist <1000 (bp)> /partitions <-1> /out <out.DIR>]")>
+               Usage:="/Palindrome.BatchTask /in <in.DIR> [/num_threads 4 /min 3 /max 20 /min-appears 2 /cutoff <0.6> /Palindrome /max-dist <1000 (bp)> /partitions <-1> /out <out.DIR>]")>
+    <ParameterInfo("/Palindrome", True, Description:="Only search for Palindrome, not includes the repeats data.")>
     Public Function PalindromeBatchTask(args As CommandLine.CommandLine) As Integer
         Dim inDIR As String = args - "/in"
         Dim min As Integer = args.GetValue("/min", 3)
@@ -25,9 +26,10 @@ Partial Module Utilities
         Dim maxDist As Integer = args.GetValue("/max-dist", 1000)
         Dim parts As Integer = args.GetValue("/partitions", -1)
         Dim minAp As Integer = args.GetValue("/min-appears", 2)
+        Dim onlyPalindrome As String = If(args.GetBoolean("/Palindrome"), "/Palindrome", "")
         Dim task As Func(Of String, String) =
             Function(fa) _
-                $"{api} /in {fa.CliPath} /min {min} /max {max} /min-appears {minAp} /out {out.CliPath} /cutoff {cutoff} /max-dist {maxDist} /partitions {parts} /batch"
+                $"{api} /in {fa.CliPath} /min {min} /max {max} /min-appears {minAp} /out {out.CliPath} /cutoff {cutoff} /max-dist {maxDist} /partitions {parts} /batch {onlyPalindrome}"
         Dim CLI As String() = files.ToArray(task)
 
         Return App.SelfFolks(CLI, parallel:=n)
@@ -39,9 +41,10 @@ Partial Module Utilities
     ''' <param name="args"></param>
     ''' <returns></returns>
     <ExportAPI("/Palindrome.Workflow",
-               Usage:="/Palindrome.Workflow /in <in.fasta> [/batch /min-appears 2 /min 3 /max 20 /cutoff <0.6> /max-dist <1000 (bp)> /partitions <-1> /out <out.DIR>]")>
+               Usage:="/Palindrome.Workflow /in <in.fasta> [/batch /min-appears 2 /min 3 /max 20 /cutoff <0.6> /max-dist <1000 (bp)> /Palindrome /partitions <-1> /out <out.DIR>]")>
     <ParameterInfo("/in", False,
                    Description:="This is a single sequence fasta file.")>
+    <ParameterInfo("/Palindrome", True, Description:="Only search for Palindrome, not includes the repeats data.")>
     Public Function PalindromeWorkflow(args As CommandLine.CommandLine) As Integer
         Dim [in] As String = args("/in")
         Dim min As Integer = args.GetValue("/min", 3)
@@ -52,11 +55,6 @@ Partial Module Utilities
         Dim minAp As Integer = args.GetValue("/min-appears", 2)
 
         Dim mirrorPalindrome As PalindromeLoci() = Topologically.SearchMirror(nt, min, max)   ' 镜像回文
-        Dim repeats As Topologically.Repeats() = RepeatsSearchAPI.SearchRepeats(nt, min, max, minAp) ' 简单重复
-        Dim rev As RevRepeats() = RepeatsSearchAPI.SearchReversedRepeats(nt, min, max, minAp) ' 反向重复
-
-        Dim repeatsViews = RepeatsView.TrimView(Topologically.Repeats.CreateDocument(repeats)).Trim(min, max, minAp)  ' 简单重复
-        Dim revViews = RevRepeatsView.TrimView(rev).Trim(min, max, minAp)     ' 反向重复
 
         Dim palindrome = Topologically.SearchPalindrome(nt, min, max)  ' 简单回文
 
@@ -68,22 +66,18 @@ Partial Module Utilities
         Dim imperfectPalindrome As Topologically.ImperfectPalindrome() = imPalSearch.ResultSet   ' 非完全回文
 
         Dim MirrorLocis = mirrorPalindrome.ToLocis
-        Dim RepeatLocis = repeats.ToLocis
-        Dim revRepeatlocis = rev.ToLocis
         Dim palindromeLocis = palindrome.ToLocis
         Dim imPalLocis = imperfectPalindrome.ToLocis
         Dim name As String = [in].BaseName
 
         If isBatch Then
             Call mirrorPalindrome.SaveTo(out & $"/MirrorPalindrome/{name}.Csv")
-            Call repeatsViews.SaveTo(out & $"/SimpleRepeats/{name}.Csv")
-            Call revViews.SaveTo(out & $"/ReversedRepeats/{name}.Csv")
+
             Call palindrome.SaveTo(out & $"/Palindrome/{name}.Csv")
             Call imperfectPalindrome.SaveTo(out & $"/ImperfectPalindrome/{name}.csv")
 
             Call MirrorLocis.SaveTo(out & $"/Sites-MirrorPalindrome/{name}.csv")
-            Call RepeatLocis.SaveTo(out & $"/Sites-SimpleRepeats/{name}.Csv")
-            Call revRepeatlocis.SaveTo(out & $"/Sites-ReversedRepeats/{name}.Csv")
+
             Call palindromeLocis.SaveTo(out & $"/Sites-Palindrome/{name}.Csv")
             Call imPalLocis.SaveTo(out & $"/Sites-ImperfectPalindrome/{name}.csv")
         Else
@@ -91,16 +85,39 @@ Partial Module Utilities
             Dim prefix As String = out & "/" & name
 
             Call mirrorPalindrome.SaveTo(prefix & ".MirrorPalindrome.Csv")
-            Call repeatsViews.SaveTo(prefix & ".SimpleRepeats.Csv")
-            Call revViews.SaveTo(prefix & ".ReversedRepeats.Csv")
+
             Call palindrome.SaveTo(prefix & ".Palindrome.Csv")
             Call imperfectPalindrome.SaveTo(prefix & ".ImperfectPalindrome.csv")
 
             Call MirrorLocis.SaveTo(prefix & ".Sites-MirrorPalindrome.csv")
-            Call RepeatLocis.SaveTo(prefix & ".Sites-SimpleRepeats.Csv")
-            Call revRepeatlocis.SaveTo(prefix & ".Sites-ReversedRepeats.Csv")
+
             Call palindromeLocis.SaveTo(prefix & ".Sites-Palindrome.Csv")
             Call imPalLocis.SaveTo(prefix & ".Sites-ImperfectPalindrome.csv")
+        End If
+
+        If Not args.GetBoolean("/Palindrome") Then
+            Dim repeats As Topologically.Repeats() = RepeatsSearchAPI.SearchRepeats(nt, min, max, minAp) ' 简单重复
+            Dim rev As RevRepeats() = RepeatsSearchAPI.SearchReversedRepeats(nt, min, max, minAp) ' 反向重复
+
+            Dim repeatsViews = RepeatsView.TrimView(Topologically.Repeats.CreateDocument(repeats)).Trim(min, max, minAp)  ' 简单重复
+            Dim revViews = RevRepeatsView.TrimView(rev).Trim(min, max, minAp)     ' 反向重复
+
+            Dim RepeatLocis = repeats.ToLocis
+            Dim revRepeatlocis = rev.ToLocis
+
+            If isBatch Then
+                Call repeatsViews.SaveTo(out & $"/SimpleRepeats/{name}.Csv")
+                Call revViews.SaveTo(out & $"/ReversedRepeats/{name}.Csv")
+                Call RepeatLocis.SaveTo(out & $"/Sites-SimpleRepeats/{name}.Csv")
+                Call revRepeatlocis.SaveTo(out & $"/Sites-ReversedRepeats/{name}.Csv")
+            Else
+                Dim prefix As String = out & "/" & name
+
+                Call repeatsViews.SaveTo(prefix & ".SimpleRepeats.Csv")
+                Call revViews.SaveTo(prefix & ".ReversedRepeats.Csv")
+                Call RepeatLocis.SaveTo(prefix & ".Sites-SimpleRepeats.Csv")
+                Call revRepeatlocis.SaveTo(prefix & ".Sites-ReversedRepeats.Csv")
+            End If
         End If
 
         Return 0
