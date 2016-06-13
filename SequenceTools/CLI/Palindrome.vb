c750281011f0efc4a809cf4687471b20c20f9bd9
xieguigang <xie.guigang@gcmodeller.org>
2016-06-13 19:28:04 +0800

    batch fuzzy mirrors searchs

--- SequenceTools/CLI/Palindrome.vb
+++ SequenceTools/CLI/Palindrome.vb
@@ -110,6 +110,31 @@ Partial Module Utilities
         Return 0
     End Function
 
+    <ExportAPI("/Mirror.Fuzzy.Batch",
+               Usage:="/Mirror.Fuzzy.Batch /in <in.fasta> [/out <out.DIR> /cut 0.6 /max-dist 6 /min 3 /max 20 /num_threads <-1>]")>
+    Public Function FuzzyMirrorsBatch(args As CommandLine.CommandLine) As Integer
+        Dim [in] As String = args - "/in"
+        Dim cut As Double = args.GetValue("/cut", 0.6)
+        Dim maxDist As Integer = args.GetValue("/max-dist", 6)
+        Dim min As Integer = args.GetValue("/min", 3)
+        Dim max As Integer = args.GetValue("/max", 20)
+        Dim out As String = args.GetValue("/out", [in].TrimFileExt & $".cut,{cut}-dist,{maxDist}-min,max={min},{max}/")
+        Dim nt As New FastaFile([in])
+        Dim CLI As New List(Of String)
+        Dim n As Integer = args.GetValue("/num_threads", -1)
+
+        For Each fa As FastaToken In nt
+            Dim tmp As String = App.GetAppSysTempFile(".fasta")
+            Dim path As String = out & "/" & fa.Title.NormalizePathString(True).Replace(" ", "_") & ".Csv"
+
+            Call fa.SaveTo(tmp)
+
+            CLI += $"{GetType(Utilities).API(NameOf(FuzzyMirrors))} /in {tmp.CliPath} /out {path.CliPath} /cut {cut} /max-dist {maxDist} /min {min} /max {max}"
+        Next
+
+        Return App.SelfFolks(CLI, LQuerySchedule.AutoConfig(n))
+    End Function
+
     <ExportAPI("/Mirror.Batch",
                Usage:="/Mirror.Batch /nt <nt.fasta> [/out <out.csv> /mp /min <3> /max <20> /num_threads <-1>]")>
     <ParameterInfo("/mp", True,
