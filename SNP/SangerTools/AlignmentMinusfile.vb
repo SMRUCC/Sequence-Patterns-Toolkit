80faf2f782e442e8168c433420d738c64854b31e
xieguigang <xie.guigang@gcmodeller.org>
2016-06-15 13:25:35 +0800

    detects SNPs

---
+++ SNP/SangerTools/AlignmentMinusfile.vb
@@ -0,0 +1,116 @@
+﻿Imports Microsoft.VisualBasic.Language.C
+Imports System.Diagnostics
+Imports System.IO
+Imports FILE = System.IO.StreamWriter
+Imports System.Runtime.CompilerServices
+Imports LANS.SystemsBiology.SequenceModel.FASTA
+
+Public Module AlignmentMinusfile
+
+    Public number_of_samples As Integer
+    Public number_of_snps As Integer
+    Public sequence_names As String()
+    Public snp_locations As Integer()
+    Public pseudo_reference_sequence As Char()
+    Public length_of_genome As Integer
+
+    Public Sub get_bases_for_each_snp(ByRef filename As String, ByRef bases_for_snps As String())
+        'Dim l As Integer
+        'Dim i As Integer = 0
+        'Dim sequence_number As Integer = 0
+        'Dim length_of_genome_found As UInteger = 0
+
+
+        'While (InlineAssignHelper(l, GlobalMembersAlignmentMinusfile.kseq_read(seq))) >= 0
+        '    If sequence_number = 0 Then
+        '        length_of_genome_found = seq.seq.l
+        '    End If
+        '    For i = 0 To number_of_snps - 1
+        '        bases_for_snps(i)(sequence_number) = Char.ToUpper(DirectCast(seq.seq.s, String)(snp_locations(i)))
+        '    Next
+
+        '    If seq.seq.l <> length_of_genome_found Then
+        '        StdErr.Write("Alignment %s contains sequences of unequal length. Expected length is %i but got %i in sequence %s" & vbLf & vbLf, filename, CInt(length_of_genome_found), CInt(seq.seq.l), seq.name.s)
+        '        Environment.[Exit](1)
+        '    End If
+        '    sequence_number += 1
+        'End While
+    End Sub
+
+    ''' <summary>
+    ''' Detection of the SNP sites based on a set of fasta sequence.
+    ''' </summary>
+    ''' <param name="filename">The input fasta sequence.</param>
+    ''' <param name="pure_mode"></param>
+    ''' <param name="output_monomorphic"></param>
+    Public Sub DetectSNPs(ByRef filename As String, pure_mode As Integer, output_monomorphic As Integer)
+        Dim first_sequence As String = Nothing
+        Dim fasta As New FastaFile(filename)
+
+        number_of_snps = 0
+        number_of_samples = 0
+        length_of_genome = 0
+        sequence_names = New String(DefineConstants.DEFAULT_NUM_SAMPLES - 1) {}
+
+        For Each fa As FastaToken In fasta
+
+            If number_of_samples = 0 Then
+                length_of_genome = fa.Length
+                Memset(first_sequence, "N"c, length_of_genome)
+                Memset(pseudo_reference_sequence, "N"c, length_of_genome)
+            End If
+
+            Dim seq As Char() = fa.SequenceData.ToCharArray
+
+            For i As Integer = 0 To length_of_genome - 1
+                If first_sequence(i) = "#"c Then
+                    Continue For
+                End If
+
+                If first_sequence(i) = "N"c AndAlso IsUnknown(fa.SequenceData(i)) = 0 Then
+                    first_sequence = CString.ChangeCharacter(first_sequence, i, Char.ToUpper(seq(i)))
+                    pseudo_reference_sequence(i) = Char.ToUpper(seq(i))
+                End If
+
+                ' in pure mode we only want /ACGT/i, if any other base is found the whole column is excluded
+                If pure_mode <> 0 AndAlso IsPure(seq(i)) = 0 Then
+                    first_sequence = CString.ChangeCharacter(first_sequence, i, "#"c)
+                    Continue For
+                End If
+
+                If first_sequence(i) <> ">"c AndAlso IsUnknown(seq(i)) = 0 AndAlso first_sequence(i) <> "N"c AndAlso first_sequence(i) <> Char.ToUpper(seq(i)) Then
+                    first_sequence = CString.ChangeCharacter(first_sequence, i, ">"c)
+                End If
+            Next
+
+            If number_of_samples >= DefineConstants.DEFAULT_NUM_SAMPLES Then
+            End If
+
+            sequence_names(number_of_samples) = fa.Title
+            number_of_samples += 1
+        Next
+
+        For i = 0 To length_of_genome - 1
+            If first_sequence(i) = ">"c OrElse (output_monomorphic <> 0 AndAlso first_sequence(i) <> "#"c) Then
+                number_of_snps += 1
+            End If
+        Next
+
+        If number_of_snps = 0 Then
+            StdErr.Write(NoSNPs & vbLf)
+            StdErr.Flush()
+            Environment.[Exit](1)
+        End If
+
+        Dim current_snp_index As Integer = 0
+
+        For i = 0 To length_of_genome - 1
+            If first_sequence(i) = ">"c OrElse (output_monomorphic <> 0 AndAlso first_sequence(i) <> "#"c) Then
+                snp_locations(current_snp_index) = i
+                current_snp_index += 1
+            End If
+        Next
+    End Sub
+
+    Const NoSNPs As String = "Warning: No SNPs were detected so there is nothing to output."
+End Module
---
+++ SNP/SangerTools/SNPSites.vb
@@ -0,0 +1,132 @@
+''' <summary>
+''' SNP functions entry point at here
+''' </summary>
+Public Module SNPSites
+
+    Private Function __snpSitesGeneric(ByRef filename As String,
+                                       output_multi_fasta_file As Integer,
+                                       output_vcf_file As Integer,
+                                       output_phylip_file As Integer,
+                                       ByRef output_filename As String,
+                                       output_reference As Integer,
+                                       pure_mode As Integer,
+                                       output_monomorphic As Integer) As Integer
+
+        AlignmentMinusfile.DetectSNPs(filename, pure_mode, output_monomorphic)
+
+        Dim bases_for_snps As String() = New String(AlignmentMinusfile.number_of_snps - 1) {}
+
+        AlignmentMinusfile.get_bases_for_each_snp(filename, bases_for_snps)
+
+        Dim output_filename_base As New String(New Char(FILENAME_MAX - 1) {})
+        Dim filename_without_directory As New String(New Char(FILENAME_MAX - 1) {})
+
+        output_filename_base = filename_without_directory.Substring(0, FILENAME_MAX)
+
+        If output_filename IsNot Nothing AndAlso output_filename <> ControlChars.NullChar Then
+            output_filename_base = output_filename.Substring(0, FILENAME_MAX)
+        End If
+
+        If output_vcf_file <> 0 Then
+            Dim vcf_output_filename As New String(New Char(FILENAME_MAX - 1) {})
+            vcf_output_filename = output_filename_base.Substring(0, FILENAME_MAX)
+            If (output_vcf_file + output_phylip_file + output_multi_fasta_file) > 1 OrElse (output_filename Is Nothing OrElse output_filename = ControlChars.NullChar) Then
+                vcf_output_filename += ".vcf"
+            End If
+
+            Vcf.create_vcf_file(vcf_output_filename,
+                                AlignmentMinusfile.snp_locations,
+                                AlignmentMinusfile.number_of_snps,
+                                bases_for_snps,
+                                AlignmentMinusfile.sequence_names,
+                                AlignmentMinusfile.number_of_samples,
+                                AlignmentMinusfile.length_of_genome,
+                                AlignmentMinusfile.pseudo_reference_sequence)
+        End If
+
+
+        If output_phylip_file <> 0 Then
+            Dim phylip_output_filename As New String(New Char(FILENAME_MAX - 1) {})
+            phylip_output_filename = output_filename_base.Substring(0, FILENAME_MAX)
+            If (output_vcf_file + output_phylip_file + output_multi_fasta_file) > 1 OrElse (output_filename Is Nothing OrElse output_filename = ControlChars.NullChar) Then
+                phylip_output_filename += ".phylip"
+            End If
+
+            GlobalMembersPhylibMinusofMinussnpMinussites.create_phylib_of_snp_sites(
+                phylip_output_filename,
+                AlignmentMinusfile.number_of_snps,
+                bases_for_snps,
+                AlignmentMinusfile.sequence_names,
+                AlignmentMinusfile.number_of_samples,
+                output_reference,
+                AlignmentMinusfile.pseudo_reference_sequence,
+                AlignmentMinusfile.snp_locations)
+        End If
+
+        If (output_multi_fasta_file) OrElse (output_vcf_file = 0 AndAlso output_phylip_file = 0 AndAlso output_multi_fasta_file = 0) Then
+            Dim multi_fasta_output_filename As New String(New Char(FILENAME_MAX - 1) {})
+            multi_fasta_output_filename = output_filename_base.Substring(0, FILENAME_MAX)
+            If (output_vcf_file + output_phylip_file + output_multi_fasta_file) > 1 OrElse (output_filename Is Nothing OrElse output_filename = ControlChars.NullChar) Then
+                multi_fasta_output_filename += ".snp_sites.aln"
+            End If
+
+            GlobalMembersFastaMinusofMinussnpMinussites.create_fasta_of_snp_sites(
+                multi_fasta_output_filename,
+                AlignmentMinusfile.number_of_snps,
+                bases_for_snps,
+                AlignmentMinusfile.sequence_names,
+                AlignmentMinusfile.number_of_samples,
+                output_reference,
+                AlignmentMinusfile.pseudo_reference_sequence,
+                AlignmentMinusfile.snp_locations)
+        End If
+
+        Return 1
+    End Function
+
+    Public Function OutputSNPSites(ByRef filename As String,
+                                   output_multi_fasta_file As Integer,
+                                   output_vcf_file As Integer,
+                                   output_phylip_file As Integer,
+                                   ByRef output_filename As String) As Integer
+
+        Return __snpSitesGeneric(filename,
+                                 output_multi_fasta_file,
+                                 output_vcf_file,
+                                 output_phylip_file,
+                                 output_filename, 0, 0, 0)
+    End Function
+
+    Public Function OutputSNPSitesWithRef(ByRef filename As String,
+                                          output_multi_fasta_file As Integer,
+                                          output_vcf_file As Integer,
+                                          output_phylip_file As Integer,
+                                          ByRef output_filename As String) As Integer
+
+        Return __snpSitesGeneric(filename,
+                                 output_multi_fasta_file,
+                                 output_vcf_file,
+                                 output_phylip_file,
+                                 output_filename, 1, 0, 0)
+    End Function
+
+    Public Function OutputSNPSitesWithRefPureMono(ByRef filename As String,
+                                                  output_multi_fasta_file As Integer,
+                                                  output_vcf_file As Integer,
+                                                  output_phylip_file As Integer,
+                                                  ByRef output_filename As String,
+                                                  output_reference As Integer,
+                                                  pure_mode As Integer,
+                                                  output_monomorphic As Integer) As Integer
+
+        Return __snpSitesGeneric(filename,
+                                 output_multi_fasta_file,
+                                 output_vcf_file,
+                                 output_phylip_file,
+                                 output_filename,
+                                 output_reference,
+                                 pure_mode,
+                                 output_monomorphic)
+    End Function
+End Module
+
