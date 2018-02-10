77e1b0997220e4157825512526c461625566211d
谢桂纲 <xie.guigang@gcmodeller.org>
2018-02-11 01:33:56 +0800

    how to populate Probability models

--- MotifScanner/test/Module1.vb
+++ MotifScanner/test/Module1.vb
@@ -5,8 +5,20 @@ Module Module1
 
     Sub Main()
 
+        Call scanerTest()
+
         Call loadTest()
     End Sub
+
+    Sub scanerTest()
+
+        Dim orgs = "P:\XCC\models".LoadKEGGModels
+        Dim models = ModelLoader.LoadGenomic("P:\XCC\assembly", "P:\XCC\models").ToArray
+
+        Dim scaner As New ConsensusScanner(models)
+
+    End Sub
+
 
     Sub loadTest()
         Dim orgs = "P:\XCC\models".LoadKEGGModels
