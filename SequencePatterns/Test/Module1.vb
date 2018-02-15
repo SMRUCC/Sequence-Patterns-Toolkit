afd5d35c413ca0eb79fdc95e644cec18a9f1eea1
谢桂纲 <xie.guigang@gcmodeller.org>
2018-02-16 02:42:11 +0800

    config project

--- SequencePatterns/Test/Module1.vb
+++ SequencePatterns/Test/Module1.vb
@@ -1,48 +1,47 @@
 ﻿#Region "Microsoft.VisualBasic::c7cfe5201ac6d2f9ced5a008799ea14f, analysis\SequenceToolkit\SequencePatterns\Test\Module1.vb"
 
-    ' Author:
-    ' 
-    '       asuka (amethyst.asuka@gcmodeller.org)
-    '       xie (genetics@smrucc.org)
-    '       xieguigang (xie.guigang@live.com)
-    ' 
-    ' Copyright (c) 2018 GPL3 Licensed
-    ' 
-    ' 
-    ' GNU GENERAL PUBLIC LICENSE (GPL3)
-    ' 
-    ' 
-    ' This program is free software: you can redistribute it and/or modify
-    ' it under the terms of the GNU General Public License as published by
-    ' the Free Software Foundation, either version 3 of the License, or
-    ' (at your option) any later version.
-    ' 
-    ' This program is distributed in the hope that it will be useful,
-    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    ' GNU General Public License for more details.
-    ' 
-    ' You should have received a copy of the GNU General Public License
-    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+' Author:
+' 
+'       asuka (amethyst.asuka@gcmodeller.org)
+'       xie (genetics@smrucc.org)
+'       xieguigang (xie.guigang@live.com)
+' 
+' Copyright (c) 2018 GPL3 Licensed
+' 
+' 
+' GNU GENERAL PUBLIC LICENSE (GPL3)
+' 
+' 
+' This program is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+' 
+' This program is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+' 
+' You should have received a copy of the GNU General Public License
+' along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module Module1
-    ' 
-    '     Sub: Main
-    ' 
-    ' /********************************************************************************/
+' Module Module1
+' 
+'     Sub: Main
+' 
+' /********************************************************************************/
 
 #End Region
 
-Imports LANS.SystemsBiology.AnalysisTools.SequenceTools.SequencePatterns
-Imports LANS.SystemsBiology.AnalysisTools.SequenceTools.SequencePatterns.Motif
-Imports LANS.SystemsBiology.AnalysisTools.SequenceTools.SequencePatterns.Motif.Patterns
-Imports LANS.SystemsBiology.SequenceModel.FASTA
+Imports SMRUCC.genomics.Analysis.SequenceTools.SequencePatterns.Abstract.Motif
+Imports SMRUCC.genomics.Analysis.SequenceTools.SequencePatterns.Abstract.Motif.Patterns
+Imports SMRUCC.genomics.SequenceModel.FASTA
 
 Module Module1
 
@@ -54,7 +53,7 @@ Module Module1
 
 
 
-        Dim scan As New Scanner(New FastaToken("F:\Xanthomonas_campestris_8004_uid15\CP000050.fna"))
+        Dim scan As New Scanner(New FastaSeq("F:\Xanthomonas_campestris_8004_uid15\CP000050.fna"))
         Dim result = scan.Scan(s)
 
         Dim motif As String = "[AG]{2,7}at*g+G4A{3,5}G{29}N?N{x}-(aa{x}TGA{b}){3,7}~x={2,5};b={x+2}"
