18408bb7b3fc10274e0afc71633067072b2aa4a8
xieguigang <xie.guigang@gcmodeller.org>
2020-05-16 23:06:28 +0800

    parse data models

---
+++ MotifScanner/Report/ReportParser.vb
@@ -0,0 +1,21 @@
+﻿Public Module ReportParser
+
+    Public Iterator Function ParseReport(text As String) As IEnumerable(Of GeneReport)
+        For Each section As String() In text _
+            .LineTokens _
+            .Split(
+                delimiter:=Function(l) l.FirstOrDefault = ">"c,
+                deliPosition:=DelimiterLocation.NextFirst
+            ) _
+            .Where(Function(sec)
+                       Return Not sec.IsNullOrEmpty
+                   End Function)
+
+            Yield ParseSectionData(section)
+        Next
+    End Function
+
+    Private Function ParseSectionData(lines As String()) As GeneReport
+
+    End Function
+End Module
